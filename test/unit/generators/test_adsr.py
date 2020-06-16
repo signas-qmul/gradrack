276984b5ac3caa2f6135d975495aaca0f24f3817
Ben Hayes <ben@benhayes.net>
2020-06-16 17:01:18 +0100

    Begin refactoring ADSR tests

--- test/unit/generators/test_adsr.py
+++ test/unit/generators/test_adsr.py
@@ -144,30 +144,49 @@ class TestADSREnvelope:
         )
 
     def test_generates_multiple_envelopes_when_release_interrupts_attack(self):
-        self.check_correctly_generates_envelope(
-            torch.Tensor([0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1]),
-            torch.Tensor([4]),
-            torch.Tensor([120]),
-            torch.Tensor([0.2]),
-            torch.Tensor([4]),
-            None,
-            torch.Tensor([
-                0.0,
-                0.25,
-                0.5,
-                0.5 * (1 / math.e ** 0.25) ** 1,
-                0.5 * (1 / math.e ** 0.25) ** 2,
-                0.5 * (1 / math.e ** 0.25) ** 3,
-                (1 + 1.5 * (1 / math.e ** 0.25) ** 3) / 4,
-                (2 + 1 * (1 / math.e ** 0.25) ** 3) / 4,
-                (((2 + 1 * (1 / math.e ** 0.25) ** 3) / 4)
-                    * (1 / math.e ** 0.25) ** 1),
-                (((2 + 1 * (1 / math.e ** 0.25) ** 3) / 4)
-                    * (1 / math.e ** 0.25) ** 2),
-                0.3748915291 + (1 - 0.3748915291) / 4,
-                0.3748915291 + 2 * (1 - 0.3748915291) / 4,
-            ])
+        gate = torch.Tensor([0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1])
+
+        attack = 4
+        decay = 120
+        sustain = 0.2
+        release = 4
+        alpha_d, alpha_r = self._compute_time_constants(decay, release)
+
+        initial_section = torch.Tensor([0])
+        attack_section_1 = self._generate_attack_portion(0, attack, 2)
+        release_section_1 = self._generate_exponential_decay(
+            attack_section_1[-1],
+            alpha_r,
+            3)
+        attack_section_2 = self._generate_attack_portion(
+            release_section_1[-1],
+            attack,
+            2)
+        release_section_2 = self._generate_exponential_decay(
+            attack_section_2[-1],
+            alpha_r,
+            2
         )
+        attack_section_3 = self._generate_attack_portion(
+            release_section_2[-1],
+            attack,
+            2)
+        expected_output = torch.cat((
+            initial_section,
+            attack_section_1,
+            release_section_1,
+            attack_section_2,
+            release_section_2,
+            attack_section_3))
+
+        self.check_correctly_generates_envelope(
+            gate,
+            attack,
+            decay,
+            sustain,
+            release,
+            None,
+            expected_output)
 
     def test_generates_envelope_when_release_interrupts_longer_attack(self):
         alpha_r = 1 / math.e ** 0.125  # release time constant
@@ -186,10 +205,11 @@ class TestADSREnvelope:
             torch.Tensor([8]),
             None,
             torch.Tensor([
-                0.1,
-                attack_1_final_value,
-                attack_1_final_value * alpha_r ** 1,
-                release_1_final_value,
+                0.1,  # attack 1 begin
+                attack_1_final_value,  # attack_1 end
+                attack_1_final_value * alpha_r ** 1,  # release 1 begin
+                release_1_final_value,  # release 1 end
+                #  attack 2 begin:
                 release_1_final_value + 1 * (1 - release_1_final_value) / 10,
                 release_1_final_value + 2 * (1 - release_1_final_value) / 10,
                 release_1_final_value + 3 * (1 - release_1_final_value) / 10,
@@ -197,15 +217,19 @@ class TestADSREnvelope:
                 release_1_final_value + 5 * (1 - release_1_final_value) / 10,
                 release_1_final_value + 6 * (1 - release_1_final_value) / 10,
                 release_1_final_value + 7 * (1 - release_1_final_value) / 10,
-                attack_2_final_value,
-                attack_2_final_value * alpha_r ** 1,
+                attack_2_final_value,  # attack 2 end
+                attack_2_final_value * alpha_r ** 1,  # release 2 begin
                 attack_2_final_value * alpha_r ** 2,
                 attack_2_final_value * alpha_r ** 3,
-                attack_2_final_value * alpha_r ** 4
+                attack_2_final_value * alpha_r ** 4  # release 2 end
             ])
         )
 
     def test_converts_from_seconds_to_samples_when_sample_rate_given(self):
+        alpha_d = 1 / math.e ** 0.5  # expected decay time constant
+        alpha_r = 1 / math.e ** 0.25  # expected releae time constant
+        decay_final_value = alpha_d ** 2
+
         self.check_correctly_generates_envelope(
             torch.Tensor([0, 1, 1, 1, 1, 0, 0]),
             torch.Tensor([0.5]),
@@ -215,12 +239,12 @@ class TestADSREnvelope:
             4,
             torch.Tensor([
                 0,
-                0.5,
-                1.0,
-                (1 / math.e ** 0.5),
-                (1 / math.e ** 0.5) ** 2,
-                (1 / math.e ** 0.5) ** 2 * (1 / math.e ** 0.25),
-                (1 / math.e ** 0.5) ** 2 * (1 / math.e ** 0.25) ** 2
+                0.5,  # attack begin
+                1.0,  # attack end
+                alpha_d ** 1,  # decay begin
+                alpha_d ** 2,  # decay end
+                decay_final_value * alpha_r ** 1,  # release begin
+                decay_final_value * alpha_r ** 2   # release end
             ])
         )
 
@@ -267,3 +291,21 @@ class TestADSREnvelope:
             torch.Tensor([0]),
             torch.Tensor([1]),
             None)
+
+    def _compute_time_constants(self, decay, release):
+        return (self._compute_time_constant(decay),
+                self._compute_time_constant(release))
+
+    def _compute_time_constant(self, length):
+        return 1 / math.e ** (1 / length)
+
+    def _generate_attack_portion(self, start, attack_time, length):
+        axis = torch.arange(length) + 1
+        ramp = (1 - start) * axis / float(attack_time) + start
+        return ramp
+
+    def _generate_exponential_decay(self, start, time_constant, length):
+        axis = torch.ones(length) * time_constant
+        axis[0] *= start
+        slope = axis.cumprod(-1)
+        return slope
